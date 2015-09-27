b0c0d7e2e48d153f9b43ee5639d903ff431076c4
luciusmagn <luk.hozda@gmail.com>
2015-09-27 09:31:02 +0200

    sub-commit

---
+++ Hat.NET/Utils.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hat.NET
+{
+    public static class Utils
+    {
+        public static List<string> GetCodeBlocks(string source)
+        {
+            List<string> Blocks = new List<string>();
+            string CurrentBlock = "";
+            bool isInString = false;
+            string Temp;
+            int OpenedBrackets = 0;
+            int ClosedBrackets = 0;
+            int charIndex = 0;
+            Temp = source;
+            NEXTBLOCK:
+            foreach (char ch in Temp)
+            {
+                if (And(ch == '\"' || ch == '\'', (charIndex != 0 ? source[charIndex - 1] != '\\' : true)))
+                {
+                    if (isInString) isInString = false;
+                    else isInString = true;
+                }
+                if (ch == '{' && !isInString)
+                {
+                    CurrentBlock += ch;
+                    OpenedBrackets++;
+                }
+                else if (ch == '}' && !isInString)
+                {
+                    CurrentBlock += ch;
+                    ClosedBrackets++;
+                    if (And(OpenedBrackets != 0, ClosedBrackets != 0) && OpenedBrackets == ClosedBrackets)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    CurrentBlock += ch;
+                }
+                charIndex++;
+            }
+            Temp = Temp.Replace(CurrentBlock, "");
+            Blocks.Add(CurrentBlock);
+            CurrentBlock = "";
+            if (!String.IsNullOrWhiteSpace(Temp))
+            {
+                goto NEXTBLOCK;
+            }
+            return Blocks;
+        }
+        public static bool And(bool left, bool right)
+        {
+            if (right && left)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        public static string RemoveExtraWS(string source)
+        {
+            string temp = "";
+            foreach(char ch in source)
+            {
+                char last = 'a';
+                switch (ch)
+                {
+                    case ' ':
+                    case '\r':
+                    case '\n':
+                    case ' ':
+                        if(last == ch)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            temp += ch;
+                            last = ch;
+                            continue;
+                        }
+                }
+            }
+            return temp;
+        }
+
+        public static List<string> SplitIfNotCodeBlock(string source, char c)
+        {
+            List<string> temp = new List<string>();
+            string tempstr = "";
+            bool codeblock = false;
+            bool isString = false;
+            int indexOne = 0;
+            int indexTwo = 0;
+            foreach(char ch in source)
+            {
+                switch(ch)
+                {
+                    case '{':
+                        tempstr += ch;
+                        indexOne = isString ? indexOne : indexOne++;
+                        codeblock = true;
+                        break;
+                    case '}':
+                        tempstr += ch;
+                        indexTwo = isString ? indexTwo : indexTwo++;
+                        if (indexOne == indexTwo)
+                        {
+                            codeblock = false;
+                        }
+                        break;
+                    case '\"':
+                        isString = isString ? false : true;
+                        break;
+                    default:
+                        if(ch == c && !codeblock)
+                        {
+                            temp.Add(tempstr);
+                            tempstr = "";
+                        }
+                        else
+                        {
+                            tempstr += ch;
+                        }
+                        break;
+
+                }
+            }
+            return temp;
+        }
+        public static string RemoveIndent(this string source)
+        {
+            string temp = "";
+            do
+            {
+                if(source[source.GetEnumerator().Current] == ' ')
+                {
+                    continue;
+                }
+                else
+                {
+                    temp += source[source.GetEnumerator().Current];
+                }
+            }
+            while (source.GetEnumerator().MoveNext());
+            return temp;
+        }
+        public static string WriteHTMLStub(this string source)
+        {
+
+        }
+        public static string WriteHTMLStub(string source)
+        {
+
+        }
+    }
+    public enum What
+    {
+        JS,
+        CSS,
+        UNKNOWN
+    }
+}
---
+++ Hat.NET/WCParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hat.NET
+{
+    public class WCParser
+    {
+        public static string halfassed;
+        public static List<string> UsingQuery = new List<string>();
+        public static StringBuilder BuiltHTML;
+        public static void Parse(string source, StreamWriter wr)
+        {
+            halfassed = Utils.RemoveExtraWS(source);
+            List<string> statements = Utils.SplitIfNotCodeBlock(halfassed, ';');
+            foreach(string statement in statements)
+            {
+                if(statement.RemoveIndent().StartsWith("using"))
+                {
+                    What mode = new What();
+                    switch(Path.GetExtension(statement.RemoveIndent().Substring(6)))
+                    {
+                        case "js":
+                            mode = What.JS;
+                            break;
+                        case "css":
+                            mode = What.CSS;
+                            break;
+                        default:
+                            mode = What.UNKNOWN;
+                            break;
+                    }
+                    UsingQuery.Add(string.Format("<{0} {1}={2} {3}={4}>", 
+                        (mode == What.CSS ? "link" : (mode == What.JS ? "script" : "link")), 
+                        (mode == What.CSS ? "rel" : (mode == What.JS ? "type" : "rel")), 
+                        (mode == What.CSS ? "stylesheet" : (mode == What.JS ? "text/javascript" : "text/text")), 
+                        (mode == What.CSS ? "href" : (mode == What.JS ? "src" : "href")), 
+                        statement.RemoveIndent().Substring(6)
+                    ));
+                    Console.WriteLine(string.Format("<{0} {1}={2} {3}={4}>",
+                        (mode == What.CSS ? "link" : (mode == What.JS ? "script" : "link")),
+                        (mode == What.CSS ? "rel" : (mode == What.JS ? "type" : "rel")),
+                        (mode == What.CSS ? "stylesheet" : (mode == What.JS ? "text/javascript" : "text/text")),
+                        (mode == What.CSS ? "href" : (mode == What.JS ? "src" : "href")),
+                        statement.RemoveIndent().Substring(6)
+                    ));
+                }
+                else if(statement.RemoveIndent().StartsWith("doctype"))
+                {
+                    BuiltHTML.AppendLine(string.Format("<!doctype {0}>", statement.RemoveIndent().ToLower().Replace("doctype", "").RemoveIndent()));
+                }
+            }
+        }
+    }
+}
