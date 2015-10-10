77cd3bf8b1302fb38ca336606ac4e331bd216c15
luciusmagn <luk.hozda@gmail.com>
2015-10-11 00:40:10 +0800

    Half-assed commit

--- Hat.NET/SubdomainService.cs
+++ Hat.NET/SubdomainService.cs
@@ -16,18 +16,26 @@ namespace Hat.NET
         public static SubdomainService cfg = new SubdomainService();
         public static void EntryHook(string fileName, HttpProcessor processor, HandleHaltArgs e)
         {
-            
+            if(Subdomains.ContainsValue((processor.httpHeaders["Host"] as string)))
+            {
+
+            }
         }
 
         public static void Initialize()
         {
+            bool isComment = false;
             if (File.Exists(Path.Combine(Environment.CurrentDirectory, "services", "subdomains.service")))
             {
                 foreach(string line in File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "services", "subdomains.service")))
                 {
-                    if (line.Split(new char[] { '=' }).Count() >= 2)
+                    if(line.Replace(" ", "").StartsWith("/*"))
+                        isComment = true;
+                    if (line.Trim().Contains("*/"))
+                        isComment = false;
+                    if (line.Split(new char[] { '=' }).Count() >= 2 && !(line.StartsWith("//") && line.StartsWith("*") && line.StartsWith("#")) && !isComment)
                     {
-                        Subdomains.Add(line.Split(new char[] { '=' })[0], line.Split(new char[] { '=' }).Skip(1).);
+                        Subdomains.Add(line.Split(new char[] { '=' })[0], string.Join("", line.Split(new char[] { '=' }).Skip(1)));
                     }
                 }
             }
